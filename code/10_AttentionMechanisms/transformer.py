b870173802bbfca0168466527794fa86310e9316
Rainer <rainerseventeen@outlook.com>
2026-02-28 16:17:58 +0800

    feat: Transformer with train

--- code/10_AttentionMechanisms/transformer.py
+++ code/10_AttentionMechanisms/transformer.py
@@ -6,11 +6,15 @@ import torch
 from torch import nn
 from d2l import torch as d2l
 
-from .attention import MultiHeadAttention
+try:
+    from .attention import MultiHeadAttention
+except ImportError:
+    # Support running this file directly as a script.
+    from attention import MultiHeadAttention
 
 class PositionWiseFFN(nn.Module):
     """基于位置的前馈网络, 本质实际是单隐藏层三维 MLP """
-    def __init__(self, ffn_num_input, ffn_num_hiddens, ffn_num_outputs, *kwargs):
+    def __init__(self, ffn_num_input, ffn_num_hiddens, ffn_num_outputs, **kwargs):
         super(PositionWiseFFN, self).__init__(**kwargs)
         # pytorch 中的线性层 只关心最后一个维度, 最后一维大小应该等于 in_features
         self.dense1 = nn.Linear(ffn_num_input, ffn_num_hiddens)
@@ -47,9 +51,7 @@ class EncoderBlock(nn.Module):
     def forward(self, X, valid_lens):
         Y = self.attention(X, X, X, valid_lens) # 点积所以全部一样
         Y = self.addnorm1(X, Y) # 对 多头输入 X 和输出 Y 执行
-        Y = self.ffn(Y) # 位置前馈网络
-        Y = self.addnorm2(Y) # 再来一次 Add & Norm
-        return Y
+        return self.addnorm2(Y, self.ffn(Y)) # 再来一次 Add & Norm
 
 class TransformerEncoder(nn.Module):
     """Transformer 编码器"""
@@ -67,12 +69,118 @@ class TransformerEncoder(nn.Module):
                                 norm_shape, ffn_num_input, ffn_num_hiddens,
                                 num_heads, dropout, use_bias))
     
-    def froward(self, X, valid_lens, *args):
+    def forward(self, X, valid_lens, *args):
         # 这里乘以 sqrt(d)
         X = self.pos_encoding(self.embedding(X) * math.sqrt(self.num_hiddens))
-        # self.attention_weights = [None] * len(self.blks) # 这个是为了可视化做的
+        self.attention_weights = [None] * len(self.blks) # 这个是为了可视化做的
         for i, blk in enumerate(self.blks):
-            X = blk(X, valid_lens)
-            # self.attention_weights[i] = blk.attention.attention.attention_weights
+            X = blk(X, valid_lens) # encoder 可以看到完整序列, 这里 mask 用于屏蔽 padding
+            self.attention_weights[i] = blk.attention.attention.attention_weights
 
         return X
+
+class DecoderBlock(nn.Module):
+    """Deocoder 中的第 i 个块"""
+    def __init__(self, key_size, query_size, value_size, num_hiddens,
+                 norm_shape, ffn_num_input, ffn_num_hiddens, num_heads,
+                 dropout, i, **kwargs):
+        super(DecoderBlock, self).__init__(**kwargs)
+        self.i = i
+        self.attention1 = MultiHeadAttention(
+            key_size, query_size, value_size, num_hiddens, num_heads, dropout)
+        self.addnorm1 = AddNorm(norm_shape, dropout)
+        self.attention2 =  MultiHeadAttention(
+            key_size, query_size, value_size, num_hiddens, num_heads, dropout)
+        self.addnorm2 = AddNorm(norm_shape, dropout)
+        self.ffn = PositionWiseFFN(ffn_num_input, ffn_num_hiddens, num_hiddens)
+        self.addnorm3 = AddNorm(norm_shape, dropout)
+    
+    def forward(self, X, state):
+        enc_ouputs, enc_valid_lens = state[0], state[1]
+        # 这里在做 KV cache 缓存, 只计算新增的 KV 而不是每次从头算
+        if state[2][self.i] is None:
+            key_values = X
+        else:
+            key_values = torch.cat((state[2][self.i], X), axis=1)
+        state[2][self.i] = key_values
+        if self.training:
+            batch_size, num_steps, _ = X.shape # (batch_size, token_len, num_hiddens)
+            # dec_valid_lens: (batch_size, num_steps)
+            # 这是 因果 mask 不是 padding mask, 代表 dec 能看到的位置, 所以每一句子都一样
+            dec_valid_lens = torch.arange(
+                1, num_steps + 1, device=X.device).repeat(batch_size, 1)
+        else:
+            dec_valid_lens = None
+        
+        # self attention
+        # X 来自 decoder 输入, key_values 来着 KV cache + X
+        X2 = self.attention1(X, key_values, key_values, dec_valid_lens)
+        Y = self.addnorm1(X, X2) # 输入输出之间 残差连接
+        # Y2 这个是 cross attention
+        Y2 = self.attention2(Y, enc_ouputs, enc_ouputs, enc_valid_lens)
+        Z = self.addnorm2(Y, Y2)
+        return self.addnorm3(Z, self.ffn(Z)), state
+
+
+class TransformerDecoder(nn.Module):
+    def __init__(self, vocab_size, key_size, query_size, value_size,
+                 num_hiddens, norm_shape, ffn_num_input, ffn_num_hiddens,
+                 num_heads, num_layers, dropout, **kwargs):
+        super(TransformerDecoder, self).__init__(**kwargs)
+        self.num_hiddens = num_hiddens
+        self.num_layers = num_layers
+        self.embedding = nn.Embedding(vocab_size, num_hiddens)
+        self.pos_encoding = d2l.PositionalEncoding(num_hiddens, dropout)
+        self.blks = nn.Sequential()
+        for i in range(num_layers):
+            self.blks.add_module("block" + str(i),
+                DecoderBlock(key_size, query_size, value_size, num_hiddens,
+                                norm_shape, ffn_num_input, ffn_num_hiddens,
+                                num_heads, dropout, i))
+        self.dense = nn.Linear(num_hiddens, vocab_size)
+
+    def init_state(self, enc_outputs, enc_valid_lens, *args):
+        """初始化解码状态 state"""
+        # state[0] = enc_outputs, 编码器的输出的每个 token 的表示, 也就是 KV
+        # state[1] = enc_valid_lens, 是 cross attention 用的 padding mask
+        # state[2] KV cache 预留的位置
+        return [enc_outputs, enc_valid_lens, [None] * self.num_layers]
+    
+    def forward(self, X, state):
+        X = self.pos_encoding(self.embedding(X) * math.sqrt(self.num_hiddens))
+        self._attention_weights = [[None] * len(self.blks) for _ in range(2)]
+        for i, blk in enumerate(self.blks):
+            X, state = blk(X, state)
+            # 以下是为了可视化
+            # decoder self attention weight
+            self._attention_weights[0][i] = blk.attention1.attention.attention_weights
+            # cross attention
+            self._attention_weights[1][i] = blk.attention2.attention.attention_weights
+        return self.dense(X), state
+
+    @property
+    def attention_weights(self):
+        return self._attention_weights
+
+
+if __name__ == "__main__":
+    num_hiddens, num_layers, dropout, batch_size, num_steps = 32, 2, 0.1, 64, 10
+    lr, num_epochs, device = 0.005, 200, d2l.try_gpu()
+    ffn_num_input, ffn_num_hiddens, num_heads = 32, 64, 4
+    key_size, query_size, value_size = 32, 32, 32
+    norm_shape = [32]
+
+    train_iter, src_vocab, tgt_vocab = d2l.load_data_nmt(batch_size, num_steps)
+    
+    encoder = TransformerEncoder(
+        len(src_vocab), key_size, query_size, value_size, num_hiddens,
+        norm_shape, ffn_num_input, ffn_num_hiddens, num_heads,
+        num_layers, dropout)
+    decoder = TransformerDecoder(
+        len(tgt_vocab), key_size, query_size, value_size, num_hiddens,
+        norm_shape, ffn_num_input, ffn_num_hiddens, num_heads,
+        num_layers, dropout)
+    
+    net = d2l.EncoderDecoder(encoder, decoder)
+    d2l.train_seq2seq(net, train_iter, lr, num_epochs, tgt_vocab, device)
+    d2l.plt.show()
